9249f8647266c830984e1fc56f6f609bce6dc6bd
youneshimi <ounesshimi@gmail.com>
2023-08-04 23:47:48 +0100

    three commit

--- php/contact.php
+++ php/contact.php
@@ -39,7 +39,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@example.net";
-$address = "themesdesign.in@gmail.com";
+$address = "ounesshimi@gmail.com";
 
 
 // Configuration option.
